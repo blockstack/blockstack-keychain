cd28228cd7e46ee446b4e47cf95a01f4396df6d9
Hank Stoever <hstove@gmail.com>
2019-11-25 16:31:33 -0800

    remove /dist from git

--- dist/encryption/encrypt.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-export declare function encryptMain(mnemonic: string, password: string): Promise<string>;
-export declare function encrypt(plaintextBuffer: Buffer, password: string): Promise<string>;
--- dist/utils/gaia.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const getHubInfo: (hubUrl: string) => Promise<any>;
-export declare const getHubPrefix: (hubUrl: string) => Promise<any>;
-export declare const makeGaiaAssociationToken: (secretKeyHex: string, childPublicKeyHex: string) => string;
