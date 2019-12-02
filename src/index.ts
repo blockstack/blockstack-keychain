7acd13c143c772a4450b4fdca55e49c54c1b1fa2
Hank Stoever <hstove@gmail.com>
2019-12-02 14:54:10 -0800

    export decrypt, encrypt

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Wallet from './wallet'
 export { default as Wallet } from './wallet'
+export { decrypt } from './encryption/decrypt'
+export { encrypt } from './encryption/encrypt'
 
 export default {
-  Wallet
+  Wallet,
 }
